67c30652b7edb9b65714cb43047f03878f4c07f1
Ramon Chan <rc.game.studio@gmail.com>
2018-08-05 11:58:20 +1000

    setup farseer demo

---
+++ FarseerPhysics.Demo/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bridge;
+using Bridge.Html5;
+using Microsoft.Xna.Framework;
+
+namespace FarseerPhysics.Demo
+{
+    public class App
+    {
+        public static void Main()
+        {
+            Game game = new Game();
+            game.Run();
+        }
+    }
+}
---
+++ FarseerPhysics.Demo/FarseerPhysics.Demo.js
@@ -0,0 +1,17 @@
+/**
+ * @version 1.0.0.0
+ * @copyright Copyright ©  2018
+ * @compiler Bridge.NET 17.2.0
+ */
+Bridge.assembly("FarseerPhysics.Demo", function ($asm, globals) {
+    "use strict";
+
+    Bridge.define("FarseerPhysics.Demo.App", {
+        main: function Main () {
+            var game = new Microsoft.Xna.Framework.Game();
+            game.Run();
+        }
+    });
+});
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICJGYXJzZWVyUGh5c2ljcy5EZW1vLmpzIiwKICAic291cmNlUm9vdCI6ICIiLAogICJzb3VyY2VzIjogWyJBcHAuY3MiXSwKICAibmFtZXMiOiBbIiJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7Ozs7WUFhWUEsV0FBWUEsSUFBSUE7WUFDaEJBIiwKICAic291cmNlc0NvbnRlbnQiOiBbInVzaW5nIFN5c3RlbTtcclxudXNpbmcgU3lzdGVtLkNvbGxlY3Rpb25zLkdlbmVyaWM7XHJcbnVzaW5nIFN5c3RlbS5MaW5xO1xyXG51c2luZyBCcmlkZ2U7XHJcbnVzaW5nIEJyaWRnZS5IdG1sNTtcclxudXNpbmcgTWljcm9zb2Z0LlhuYS5GcmFtZXdvcms7XHJcblxyXG5uYW1lc3BhY2UgRmFyc2VlclBoeXNpY3MuRGVtb1xyXG57XHJcbiAgICBwdWJsaWMgY2xhc3MgQXBwXHJcbiAgICB7XHJcbiAgICAgICAgcHVibGljIHN0YXRpYyB2b2lkIE1haW4oKVxyXG4gICAgICAgIHtcclxuICAgICAgICAgICAgR2FtZSBnYW1lID0gbmV3IEdhbWUoKTtcclxuICAgICAgICAgICAgZ2FtZS5SdW4oKTtcclxuICAgICAgICB9XHJcbiAgICB9XHJcbn0iXQp9Cg==
