1bff9155a89b3ccd3e97b5a7179334eb92ec1064
Ramon Chan <rc.game.studio@gmail.com>
2018-08-14 20:55:43 +1000

    fullscreen

--- FarseerPhysics.Demo/App.cs
+++ FarseerPhysics.Demo/App.cs
@@ -12,8 +12,40 @@ namespace FarseerPhysics.Demo
     {
         public static void Main()
         {
-            PhysicsGame game = new PhysicsGame();
-            game.Run();
+            HTMLButtonElement button = new HTMLButtonElement();
+            button.InnerHTML = "Fullscreen Experience (use landscape)";
+            button.SetAttribute("style", 
+                @"position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 80%;
+    transform: translate(-50%, -50%);
+    font-size: 30px;");
+
+            button.OnClick = (e) =>
+            {
+                Script.Write(
+                    @"if (document.body.requestFullscreen) {
+        document.body.requestFullscreen();
+            }
+    else if (document.body.mozRequestFullScreen)
+            {
+                document.body.mozRequestFullScreen();
+            }
+            else if (document.body.webkitRequestFullscreen)
+            {
+                document.body.webkitRequestFullscreen();
+            }
+            else if (document.body.msRequestFullscreen)
+            {
+                document.body.msRequestFullscreen();
+            }
+            ");
+                Document.Body.RemoveChild(button);
+                PhysicsGame game = new PhysicsGame();
+                game.Run();
+            };
+            Document.Body.AppendChild(button);
         }
     }
 }
