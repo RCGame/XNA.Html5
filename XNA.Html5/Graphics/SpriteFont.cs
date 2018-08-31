b282e84eac2fd7875b3c489ffc480f90bb78f536
Ramon Chan <rc.game.studio@gmail.com>
2018-08-31 16:05:35 +1000

    some pre-defined stuff and drawing fix

--- XNA.Html5/Graphics/SpriteFont.cs
+++ XNA.Html5/Graphics/SpriteFont.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-    public class SpriteFont
+    public class SpriteFont : GraphicsResource
     {
         public Vector2 MeasureString(string str)
         {
-            throw new NotImplementedException();
+            return new Vector2(10, 10);
         }
     }
 }
